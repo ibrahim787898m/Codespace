ea2630391554075f74f2025f28b8398511856e81
Ibrahim Mustafa <124662761+ibrahim787898m@users.noreply.github.com>
2025-01-31 10:34:13 +0000

    modified calculator.cpp

--- cpp/calculator.cpp
+++ cpp/calculator.cpp
@@ -1,33 +1,69 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int num1, num2;
+void calculate(double num1, double num2, char op) {
+    if (op == '+') {
+        cout << "Result: " << num1 + num2 << endl;
+    } else if (op == '-') {
+        cout << "Result: " << num1 - num2 << endl;
+    } else if (op == '*') {
+        cout << "Result: " << num1 * num2 << endl;
+    } else if (op == '/') {
+        cout << "Result: " << num1 / num2 << endl;
+    }
+}
+
+int main() {    
+    double num1, num2;
     char op;
 
-    cout << "Enter first number: ";
-    cin >> num1;
+    cout << "-----------------------------------" << endl;
+
+    cout << "Simple Calculator Program" << endl;
+
+    cout << "Supported operations are +, -, *, /" << endl;
+
+    cout << "-----------------------------------" << endl;
 
     cout << "Enter operator: ";
     cin >> op;
+    if (op != '+' && op != '-' && op != '*' && op != '/') {
+        cout << "-----------------------------------" << endl;
+        cout << "Invalid operator! Please enter a valid operator and try agian." << endl;
+        cout << "-----------------------------------" << endl;
+        return 1;
+    }
+
+    cout << "Enter first number: ";
+    if (!(cin >> num1)) {
+        cout << "-----------------------------------" << endl;
+        cout << "Invalid input! Please enter a valid number and try again." << endl;
+        cout << "-----------------------------------" << endl;
+        return 1;
+    }
 
     cout << "Enter second number: ";
-    cin >> num2;
-
-    switch(op) {
-        case '+' :
-            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
-            break;
-        case '-' :
-            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
-            break;
-        case '*' :
-            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
-            break;
-        case '/' :
-            cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
-            break;
-        default :
-          cout << "Invalid operator" << endl; 
+    if (!(cin >> num2) && op != '/') {
+        cout << "-----------------------------------" << endl;
+        cout << "Ivalid input! Please enter a valid number and try again." << endl;
+        cout << "-----------------------------------" << endl;
+        return 1;
+    } else if (num2 == 0 && op == '/') {
+        cout << "-----------------------------------" << endl;
+        cout << "Error! Dibvision by zero is not allowed. Please enter a valid number and try again." << endl;
+        cout << "-----------------------------------" << endl;
+        return 1;
     }
+
+    calculate(num1, num2, op);
+
+    cout << "-----------------------------------" << endl;
+
+    cout << "Program Ended" << endl;
+
+    cout << "Thank you for using the program" << endl;
+
+    cout << "-----------------------------------" << endl;
+
+    return 0;
 }
